fe608623a774722923aff4b3be8dd202bc40776a
Tschnu <josephinamarie.steinboeck@gmail.com>
2026-01-12 15:10:15 +0100

    Hier ist alles super, hier ist alles cool

--- code/app/src/main/java/com/example/myapplication/db/DailyEntryDatabase.kt
+++ code/app/src/main/java/com/example/myapplication/db/DailyEntryDatabase.kt
@@ -1,4 +1,35 @@
 package com.example.myapplication.db
 
-class DailyEntryDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [DailyEntryEntity::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class DailyEntryDatabase :  RoomDatabase() {
+    abstract fun dailyEntryDao(): DailyEntryDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: DailyEntryDatabase? = null
+
+        fun getDatabase(context: Context): DailyEntryDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DailyEntryDatabase::class.java,
+                    "daily_entry_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
