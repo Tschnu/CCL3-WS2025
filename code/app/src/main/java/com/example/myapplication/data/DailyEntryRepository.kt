b49b61bfd1a60dcb0eda19894d5f3f307686d981
L21T24 <laetitia.toth@protonmail.com>
2026-01-12 15:16:13 +0100

    Ui screen files made

--- code/app/src/main/java/com/example/myapplication/data/DailyEntryRepository.kt
+++ code/app/src/main/java/com/example/myapplication/data/DailyEntryRepository.kt
@@ -1,7 +1,26 @@
 package com.example.myapplication.data
 
 import com.example.myapplication.db.DailyEntryDao
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class DailyEntryRepository (
-    private val: DailyEntryDao ) {
+class DailyEntryRepository(
+    private val dailyEntryDao: DailyEntryDao
+) {
+
+    val dailyEntries: Flow<List<DailyEntry>> =
+        dailyEntryDao.getAllEntries().map { entities ->
+            entities.map { entity ->
+                DailyEntry(
+                    date = entity.date,
+                    painCategory = entity.painCategory,
+                    energyCategory = entity.energyCategory,
+                    moodCategory = entity.moodCategory,
+                    bloodflowCategory = entity.bloodflowCategory
+                )
+            }
+        }
+
+
+
 }
---
+++ code/app/src/main/java/com/example/myapplication/ui/screens/StatisticsPage.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.ui.screens
+
+class StatisticsPage {
+}
