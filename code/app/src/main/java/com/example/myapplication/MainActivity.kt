3597d37bb9830a7f2c7549b48120a2eaa33403e4
L21T24 <laetitia.toth@protonmail.com>
2026-01-12 13:02:47 +0100

    trying to do maybe?

--- code/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ code/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +26,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Vector7Block(
+                            modifier = Modifier.padding(
+                                start = 78.dp,
+                                top = 599.dp
+                            )
+                        )
+                    }
                 }
             }
         }
@@ -31,17 +45,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Vector7Block(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .width(340.dp)
+            .height(280.01776.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.vector_7),
+            contentDescription = "vector 7",
+            contentScale = ContentScale.None
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewVector7Block() {
     MyApplicationTheme {
-        Greeting("Android")
+        Box(modifier = Modifier.fillMaxSize()) {
+            Vector7Block(
+                modifier = Modifier.padding(
+                    start = 200.dp,
+                    top = 650.dp
+                )
+            )
+        }
     }
 }
