efbc1278db600776d97892560d950e6eb8be8fef
L21T24 <laetitia.toth@protonmail.com>
2026-01-12 14:26:25 +0100

    Daily Entry defined

--- code/app/src/main/java/com/example/myapplication/data/DailyEntry.kt
+++ code/app/src/main/java/com/example/myapplication/data/DailyEntry.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication.data
 
 data class DailyEntry(
-    val id: Int = 1
+    val date: Long,
+    val painCategory: Int,
+    val energyCategory: Int,
+    val moodCategory: Int,
+    val bloodflowCategory: Int
 )
