515645fa18286d5550d01f2f9675fc97a30d9fdb
Tschnu <josephinamarie.steinboeck@gmail.com>
2026-01-13 09:56:01 +0100

    Soup Soup Soup

--- code/app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ code/app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val GreenDark = Color(0xFF254F07)
+val GreenLight = Color(0xFF7E8406)
+
+val BrownDark = Color(0xFF3D2B1F)
+val BrownLight = Color(0xFF6F4E37)
+
+val RedDark = Color(0xFF630602)
+val RedLight = Color(0xFFE45853)
+
+val YellowStrong = Color(0xFFDEBE00)
+val YellowSoft = Color(0xFFEBD365)
+
+val Cream = Color(0xFFF6EABA)
--- code/app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ code/app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -2,33 +2,32 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 
-// Set of Material typography styles to start with
+val Quicksand = FontFamily(
+    Font(R.font.quicksand, FontWeight.Normal)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Quicksand,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Quicksand,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 22.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Quicksand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     )
-    */
 )
